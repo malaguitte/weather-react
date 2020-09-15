96d6c189580269e95f7b7527556cdbb9a6403ad0
Anderson <upmalagutti@gmail.com>
2020-09-16 00:30:42 +0200

    utils.js does not need to to export MONTHS and DAYS

--- src/utils.js
+++ src/utils.js
@@ -1,34 +1,39 @@
+const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const DAYS = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
 export const dateBuilder = (givenDate) => {
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    ];
-    const days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
-
-    const day = days[givenDate.getDay()];
+    const day = DAYS[givenDate.getDay()];
     const date = givenDate.getDate();
-    const month = months[givenDate.getMonth()];
+    const month = MONTHS[givenDate.getMonth()];
     const year = givenDate.getFullYear();
-
+    
     return `${day} ${date} ${month} ${year}`;
 }
 
+/**
+ * Rounds the given "temperature", and includes the "º C" in front of it.
+ * For instance, when the given "temperature" is 23.32, this method returns "23º C"
+ * @param {*} temperature: The temperature to be rounded.
+ */
 export const roundTemperature = (temperature) => `${Math.round(temperature)}º C`;
