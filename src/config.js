d3d9e7c3ecdff3d4d83201edafdb735da8dc95e2
Anderson <upmalagutti@gmail.com>
2020-09-16 00:32:58 +0200

    config.js does not export OPEN_WEATHER_API keys

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
-const CONFIG = {
-
-    OPEN_WEATHER_API: {
-        KEY: "YOUR_API_KEY",
-        BASE: "https://api.openweathermap.org/data/2.5/",
-        UNIT: "metric"
-    },
-
-    getQuery: (query) => `${CONFIG.OPEN_WEATHER_API.BASE}weather?q=${query}&units=${CONFIG.OPEN_WEATHER_API.UNIT}&APPID=${CONFIG.OPEN_WEATHER_API.KEY}`
+const OPEN_WEATHER_API = {
+    KEY: "YOUR_API_KEY",
+    BASE: "https://api.openweathermap.org/data/2.5/",
+    UNIT: "metric"
 };
 
+const CONFIG = {
+    getQuery: (query) => `${OPEN_WEATHER_API.BASE}weather?q=${query}&units=${OPEN_WEATHER_API.UNIT}&APPID=${OPEN_WEATHER_API.KEY}`,
+    PLACEHOLDER_TEXT: "Search...",
+    TEMPERATURE_THRESHOLD: 16
+};
 
 export default CONFIG;
